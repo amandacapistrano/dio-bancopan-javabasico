bdc0034d63210bfc2d67209bb6a15053599981e4
Amanda <amandacapistrano@outlook.com>
2023-02-26 15:39:15 -0300

    atualizacao

--- metodos/src/usuario.java
+++ metodos/src/usuario.java
@@ -1,5 +1,24 @@
-public class App {
+public class usuario {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+    
+        smartTv.ligar ();
+        System.out.println("Novo status -> TV ligada? " + smartTv.ligada);
+        smartTv.desligar ();
+        System.out.println("Novo status -> TV desligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual: " + smartTv.canal);
+      
     }
+
 }
